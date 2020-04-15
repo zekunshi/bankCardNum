e5e9974e78e636cc7838927e0128e236e8d22e0e
szkqwer <3167142424@qq.com>
2020-04-15 22:40:58 +0800

    1111

--- demo/netWork.py
+++ demo/netWork.py
@@ -7,14 +7,10 @@ import shutil
 import sys
 
 
-import numpy as np
-import tensorflow as tf
-from tensorflow.python.platform import gfile
 
 sys.path.append(os.getcwd())
 
 
-from flask import Flask, render_template, request
 
 from web.Controller import getPictureFromNet
 
